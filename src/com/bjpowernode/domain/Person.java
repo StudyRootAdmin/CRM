f3e812222d232b8d90e4ff700ba2678853a8840a
root <root@123.com>
2022-11-01 09:55:14 +0800

    123

---
+++ src/com/bjpowernode/domain/Person.java
@@ -0,0 +1,12 @@
+package com.bjpowernode.domain;
+
+/**
+ * @ClassName: Person
+ * @Description:
+ * @Author: Li
+ * @Date: 2022/11/01 09:35 周二
+ * @Version: 1.0
+ */
+public class Person {
+    private String id;
+}
